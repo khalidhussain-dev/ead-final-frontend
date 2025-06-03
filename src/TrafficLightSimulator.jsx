ffc2b506d344c1554aa177572c2ae4623020f58e
khalidhussain-dev <khalidhussainchandio9@gmail.com>
2025-06-03 11:31:43 +0500

    Added js lighting

--- src/TrafficLightSimulator.jsx
+++ src/TrafficLightSimulator.jsx
@@ -1,14 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const TrafficLightSimulator = () => {
+
+    const [color, setColor] = useState('red')
+
+    useEffect(() => {
+
+        setInterval(() => {
+            if (color == 'red') setColor('yellow');
+            else if (color == 'yellow') setColor('green');
+            else if (color == 'green') setColor('red');
+        }, 3000);
+
+    }
+    )
+
+
+
     return (
         <div className='flex w-92 flex-col items-center h-[100vh] m-auto bg-[#111121] justify-center'>
-            <h1 className='text-white text-[0.8rem] gap-2' >Traffic Light Simulator</h1>
+            <h1 className='text-white text-[0.8rem] gap-2' >Traffic Light Simulator Color: {color}</h1>
 
             <div className='flex flex-col w-22 gap-3 mt-2 p-6 rounded-2xl items-center justify-around bg-[#333]'>
-                <div className='flex rounded-full w-12 h-12 bg-red-600'></div>
-                <div className='flex rounded-full w-12 h-12 bg-yellow-600'></div>
-                <div className='flex rounded-full w-12 h-12 bg-green-600'></div>
+                <div className={`flex rounded-full w-12 h-12 bg-${color == 'red' ? 'red-600' : '[#444]'}`}></div>
+                <div className={`flex rounded-full w-12 h-12 bg-${color == 'yellow' ? 'yellow-600' : '[#444]'}`}></div>
+                <div className={`flex rounded-full w-12 h-12 bg-${color == 'green' ? 'green-600' : '[#444]'}`}></div>
 
             </div>
         </div>
